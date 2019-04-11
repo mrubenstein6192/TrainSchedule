f092a2aa2433944f8d86874d09f1c2d3d9805598
Michael Rubenstein <mrubenstein6192@gmail.com>
2019-04-11 09:26:21 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # TrainSchedule
+
+A real-time log of upcoming train times.  The user can add a train based on his/her desired destination, time, and the frequency of the train's arrival.  
+
+## Tools Used
+
+- This app is connected to a Firebase database.
+
+- HTML
+
+- CSS
+
+- Javascript/JQuery
+
+- Moment.js
+
+### Future Improvements
+
+- An automatically updating time to show live data.
+
+- Style
+
+### Author
+
+- Mike Rubenstein
---
+++ javascript/logic.js
@@ -0,0 +1,82 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBCjmE80G2BdjzV1M2yV_6tkddTKEt9pZM",
+  authDomain: "train-schedule-58ec1.firebaseapp.com",
+  databaseURL: "https://train-schedule-58ec1.firebaseio.com",
+  projectId: "train-schedule-58ec1",
+  storageBucket: "train-schedule-58ec1.appspot.com",
+  messagingSenderId: "213170129628"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+$("#train-form").on("submit", function(event) {
+  event.preventDefault();
+
+  //get form data from user input
+  var trainDataInput = {
+    train: $("#train-input").val().trim(),
+    destination: $("#destination-input").val().trim(),
+    firstTrainTime: $("#start-input").val().trim(),
+    frequency: $("#frequency-input").val().trim()
+  }
+
+  database.ref().push(trainDataInput);
+  console.log(trainDataInput);
+});
+
+//child added
+database.ref().on("child_added", function(childSnapshot) {
+  console.log("this is child_added");
+  console.log(childSnapshot.val());
+
+  var trainData = childSnapshot.val();
+
+  var tFrequency = trainData.frequency;
+  console.log("Frequency is " + tFrequency);
+
+  var firstTime = trainData.firstTrainTime;
+  console.log("First time is" + firstTime);
+ 
+  var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+  var currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+  
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+
+  //create table row
+  var $tr = $("<tr>");
+
+  //create <td> tage for the columns
+  var $tdTrain = $("<td>").text(trainData.train);
+  var $tdDestination = $("<td>").text(trainData.destination);
+  var $tdFrequency = $("<td>").text(trainData.frequency);
+
+  //finish later
+  var $tdArrival = $("<td>").text((nextTrain).format("hh:mm"));
+
+  //finish minutes away later
+  var $tdMinutesAway = $("<td>").text(tMinutesTillTrain);
+
+  //add content to what is displayed
+  $tr.append($tdTrain, $tdDestination, $tdFrequency, $tdArrival, $tdMinutesAway);
+
+  $("tbody").append($tr);
+
+});
